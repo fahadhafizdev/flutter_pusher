eb6e07c665055fbec4358bf0cb18fc16b4c27f87
fahadhafizdev <fahadhafiz.dev@gmail.com>
2022-11-18 08:49:16 +0700

    finish riset pusher

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,11 +1,70 @@
+import 'dart:developer';
+
 import 'package:get/get.dart';
+import 'package:pusher_channels_flutter/pusher_channels_flutter.dart';
 
 class HomeController extends GetxController {
   //TODO: Implement HomeController
 
   final count = 0.obs;
+
+  PusherChannelsFlutter pusher = PusherChannelsFlutter.getInstance();
+
+  void onConnectPressed() async {
+    // Remove keyboard
+
+    try {
+      await pusher.init(
+        apiKey: '1bdfd3cb48c8ba39565e',
+        cluster: "ap1",
+        // onConnectionStateChange: onConnectionStateChange,
+        // onError: onError,
+        // onSubscriptionSucceeded: onSubscriptionSucceeded,
+        onEvent: onEvent,
+        // onSubscriptionError: onSubscriptionError,
+        // onDecryptionFailure: onDecryptionFailure,
+        // onMemberAdded: onMemberAdded,
+        // onMemberRemoved: onMemberRemoved,
+        // onSubscriptionCount: onSubscriptionCount,
+        // authEndpoint: "<Your Authendpoint Url>",
+        // onAuthorizer: onAuthorizer
+      );
+      await pusher.subscribe(channelName: 'hello');
+      await pusher.connect();
+    } catch (e) {
+      log("ERROR: $e");
+    }
+  }
+
+  void onEvent(PusherEvent event) {
+    log("onEvent: $event");
+  }
+
+  Future<void> initPusher() async {
+    log('init pusher');
+    try {
+      await pusher.init(
+        apiKey: "1bdfd3cb48c8ba39565e",
+        cluster: "ap1",
+        // onEvent: onEvent,
+      );
+      await pusher.subscribe(channelName: 'presence-chatbox');
+
+      await pusher.connect();
+    } catch (e) {
+      log(e.toString());
+    }
+
+    // final myChannel = await pusher.subscribe(
+    //   channelName: "my-channel",
+    //   onEvent: onEvent,
+    // );
+  }
+
   @override
-  void onInit() {
+  void onInit() async {
+    onConnectPressed();
+
     super.onInit();
   }
 
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -8,15 +8,18 @@ class HomeView extends GetView<HomeController> {
   const HomeView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    HomeController homeController = Get.find();
     return Scaffold(
       appBar: AppBar(
         title: const Text('HomeView'),
         centerTitle: true,
       ),
-      body: Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
+      body: Obx(
+        () => Center(
+          child: Text(
+            'HomeView is working ${homeController.count.value}',
+            style: TextStyle(fontSize: 20),
+          ),
         ),
       ),
     );
